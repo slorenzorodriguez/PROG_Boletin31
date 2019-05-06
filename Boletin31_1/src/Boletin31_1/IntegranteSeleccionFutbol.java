864fcfecdf29dd06db26125592328cf41ec88358
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2019-05-06 11:34:40 +0200

    Boletin 31_1

---
+++ Boletin31_1/src/Boletin31_1/IntegranteSeleccionFutbol.java
@@ -0,0 +1,15 @@
+
+package Boletin31_1;
+
+/**
+ *
+ * @author Rafsniper
+ */
+public interface IntegranteSeleccionFutbol {
+    
+    public void concentrarse();
+    public void viajar();
+    public void entrenar();
+    public void jugarPartido();
+    
+}
